0dda72a62acd980fd71e391b89214907b834a5d1
yyz845935161 <51375977+yyz845935161@users.noreply.github.com>
2021-02-23 23:47:04 +0800

    Add files via upload

---
+++ 2.23/practice_2_23.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//输出小飞机
+//#include <stdio.h>
+//
+//int main()
+//{
+//    printf("     **     \n");
+//    printf("     **     \n");
+//    printf("************\n");
+//    printf("************\n");
+//    printf("    *  *    \n");
+//    printf("    *  *    \n");
+//
+//    return 0;
+//}
+
+//8进制和16进制
+//#include <stdio.h>
+//
+//int main()
+//{
+//    printf("0%o 0X%X", 1234, 1234);
+//    return 0;
+//}
+
+//int main()
+//{
+//    printf("\n%d", printf("Hello world!\n"));
+//}
+//
+//#include <stdio.h>
+//int main()
+//{
+//    int id;
+//    float Math;
+//    float C;
+//    float English;
+//    scanf("%d;%f,%f,%f", &id, &Math, &C, &English);
+//    printf("The each subject score of  No. %d is %.2f, %.2f, %.2f.", id, Math, C, English);
+//}
