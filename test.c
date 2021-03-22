d35a76a35c0056cef490936cf1a2ffe44d575e1c
yyz845935161 <51375977+yyz845935161@users.noreply.github.com>
2021-03-22 22:36:01 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,23 @@
+#include "SList.h"
+
+void test()
+{
+	LNode* p = NULL;
+	SListPushBack(&p, 1);
+	SListPushBack(&p, 2);
+	SListPushBack(&p, 3);
+	SListPushBack(&p, -5);
+	printSL(p);
+}
+void test02()
+{
+
+
+}
+
+int main()
+{
+	test();
+	test02();
+	return 0;
+}
